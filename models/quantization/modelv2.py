9d21d419ef11bae7ac0f04b16815b7807337e913
wonbeomjang <jtiger958@gmail.com>
2021-12-01 15:51:59 +0000

    add FloatFunctional

--- models/quantization/modelv2.py
+++ models/quantization/modelv2.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.quantized import FloatFunctional
 
 from torchvision.models.quantization.mobilenet import mobilenet_v2
 from torchvision.models.quantization.utils import _replace_relu
@@ -33,6 +34,7 @@ class QuantizableMobileHairNetV2(MobileHairNetV2):
         self.mobilenet = mobilenet_v2()
         self.quant = torch.quantization.QuantStub()
         self.dequant = torch.quantization.DeQuantStub()
+        self.f_add = FloatFunctional()
         
         _replace_relu(self.mobilenet)
         self.make_layers()
@@ -44,6 +46,28 @@ class QuantizableMobileHairNetV2(MobileHairNetV2):
         x = self.dequant(x)
         
         return x
+        
+    def _forward_implement(self, x):
+        #connet encode 4-> decode 1, encode 3-> decode 2, encode 2-> decode 3, encode 1-> decode 4
+        encode_layer1 = self.encode_layer1(x)
+        encode_layer2 = self.encode_layer2(encode_layer1)
+        encode_layer3 = self.encode_layer3(encode_layer2)
+        encode_layer4 = self.encode_layer4(encode_layer3)
+        encode_layer5 = self.encode_layer5(encode_layer4)
+
+        encode_layer4 = self.encode_to_decoder4(encode_layer4)
+        encode_layer3 = self.encode_to_decoder3(encode_layer3)
+        encode_layer2 = self.encode_to_decoder2(encode_layer2)
+        encode_layer1 = self.encode_to_decoder1(encode_layer1)
+
+        decode_layer1 = self.f_add.add(self.decode_layer1(encode_layer5), encode_layer4)
+        decode_layer2 = self.f_add.add(self.decode_layer2(decode_layer1), encode_layer3)
+        decode_layer3 = self.f_add.add(self.decode_layer3(decode_layer2), encode_layer2)
+        decode_layer4 = self.f_add.add(self.decode_layer4(decode_layer3), encode_layer1)
+        decode_layer5 = self.decode_layer5(decode_layer4)
+
+        out = self.soft_max(decode_layer5)
+        return out
         
     def fuse_model(self) -> None:
         for m in self.modules():
