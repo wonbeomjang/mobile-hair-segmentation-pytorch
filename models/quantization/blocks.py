4eebc5e9f257f5aace0a9184a96b2c12869778c5
wonbeomjang <jtiger958@gmail.com>
2021-12-04 13:38:50 +0000

    make block paranet

--- models/quantization/blocks.py
+++ models/quantization/blocks.py
@@ -1,42 +1,20 @@
 import torch.nn as nn
 from torch.quantization import fuse_modules
 
-class QuantizableLayerDepwiseEncode(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, reserve=False):
-        self.stride = int(out_channels/in_channels)
-        if reserve == True:
-            self.stride = 1
-        super(QuantizableLayerDepwiseEncode, self).__init__()
-        self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=1, padding=1, groups=in_channels),
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=self.stride),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+from ..blocks import 
 
-    def forward(self, x):
-        out = self.layer(x)
-        return out
+class QuantizableLayerDepwiseEncode(LayerDepwiseEncode):
+    def __init__(self, in_channels, out_channels, kernel_size=3, reserve=False):
+        super(QuantizableLayerDepwiseEncode, self).__init__()
         
     def fuse_model(self):
         fuse_modules(self.layer, ["0", "1", "2"], inplace=True)
         fuse_modules(self.layer, ["3", "4", "5"], inplace=True)
         
 
-class QuantizableLayerDepwiseDecode(nn.Module):
-    def __init__(self, in_channel, out_channel, kernel_size=3,  stride=1):
-        super(QuantizableLayerDepwiseDecode, self).__init__()
-        self.layer = nn.Sequential(
-            nn.Conv2d(in_channels=in_channel, out_channels=in_channel, kernel_size=kernel_size, stride=stride, padding=1, groups=in_channel),
-            nn.Conv2d(in_channels=in_channel, out_channels=out_channel, kernel_size=1, stride=stride),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, x):
-        out = self.layer(x)
-        return out
-        
+class QuantizableLayerDepwiseDecode(LayerDepwiseDecode):
+    def __init__(self, in_channels, out_channels, kernel_size=3, reserve=False):
+        super(QuantizableLayerDepwiseEncode, self).__init__()
+    
     def fuse_model(self):
         fuse_modules(self.layer, [str(1), str(2)], inplace=True)
